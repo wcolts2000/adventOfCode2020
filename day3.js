9e44dee7c04d01cd4908b0d9ba17b107fcf6721c
sean <sean@synap.io>
2020-12-03 12:41:31 -0800

    day 3 part 2, manually ran slopes and multiplied results. could have made into an array or slopes and looped it also

--- day3.js
+++ day3.js
@@ -3,7 +3,11 @@ const fs = require('fs');
 
 fs.readFile('./treesMap.txt', 'utf-8', (err, data) => {
     if (err) throw err;
-    const slope = [3,1];
+    // const slope = [1,1];
+    // const slope = [3,1];
+    // const slope = [5,1];
+    // const slope = [7,1];
+    const slope = [1,2];
     const pos = {
         x: 0,
         y: 0
@@ -25,3 +29,12 @@ fs.readFile('./treesMap.txt', 'utf-8', (err, data) => {
     }
     console.log(hits);
 });
+
+// part 2 with multiple slope checks
+// 70
+// 171
+// 48
+// 60
+// 35
+
+// console.log(70*171*48*60*35) // 1206576000
