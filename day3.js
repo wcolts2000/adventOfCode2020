698bc60032af2a61a6e50a8769affcc1ba5d1c7d
sean <sean@synap.io>
2020-12-03 20:26:25 -0800

    automated day3 part 2

--- day3.js
+++ day3.js
@@ -7,27 +7,44 @@ fs.readFile('./treesMap.txt', 'utf-8', (err, data) => {
     // const slope = [3,1];
     // const slope = [5,1];
     // const slope = [7,1];
-    const slope = [1,2];
+    // const slope = [1,2];
+    const slopes = [
+        [1,1],
+        [3,1],
+        [5,1],
+        [7,1],
+        [1,2]
+    ]
     const pos = {
         x: 0,
         y: 0
     };
+    let totals = [];
     let hits = 0;
     let lines = data.split('\n');
     let bottom = lines.length;
     let sliceWidth = lines[0].length
 
-    while (pos.y < bottom) {
-        if(pos.x >= sliceWidth) {
-            pos.x = pos.x - sliceWidth;
+    slopes.forEach((slope) => {
+        
+        while (pos.y < bottom) {
+            if(pos.x >= sliceWidth) {
+                pos.x = pos.x - sliceWidth;
+            }
+            if (lines[pos.y][pos.x] === "#") {
+                hits++;
+            };
+            pos.x+=slope[0];
+            pos.y+=slope[1];
         }
-        if (lines[pos.y][pos.x] === "#") {
-            hits++;
-        };
-        pos.x+=slope[0];
-        pos.y+=slope[1];
-    }
-    console.log(hits);
+        console.log(hits);
+        totals.push(hits);
+        hits = 0;
+        pos.x = 0;
+        pos.y = 0;
+    })
+    // console.log(totals.reduce((curr, acc) => acc *= curr, 1))
+    return totals.reduce((curr, acc) => acc *= curr, 1)
 });
 
 // part 2 with multiple slope checks
